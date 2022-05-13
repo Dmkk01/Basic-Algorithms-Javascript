29eb353dce3cfc373eb9b290122498e06999e2fa
Dominik <dmkk1@outlook.com>
2022-05-13 16:49:44 +0200

    init web-navigator

---
+++ web-navigator/script.js
@@ -0,0 +1,26 @@
+const Stack = require('./Stack.js');
+const prompt = require('prompt-sync')();
+// ------------------------------
+// Initialization
+// ------------------------------
+
+// ------------------------------
+// Helper Functions
+// ------------------------------
+
+/*
+ * The following strings are used to prompt the user
+ */
+const baseInfo = '\nEnter a url';
+const backInfo = 'B|b for back page';
+const nextInfo = 'N|n for next page';
+const quitInfo = 'Q|q for quit';
+const question = 'Where would you like to go today? '
+
+// ------------------------------
+// User Interface Part 1
+// ------------------------------
+
+  // ------------------------------
+  // User Interface Part 2
+  // ------------------------------
